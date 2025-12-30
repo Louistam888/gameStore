1e93bb7d29a542429f1725e88fad8708a40f59ec
Louistam888 <louistam888@gmail.com>
2025-12-30 14:50:10 -0500

    initial commit

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,32 @@
+using GameStore.Api.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street FighterII",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)
+        ),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)
+        ),
+    new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27)
+        ),
+];
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
